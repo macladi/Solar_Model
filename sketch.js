b54994c92272d5a3ca80edb0b0289621f57f9786
macladi <dcastillonsoler@gmail.com>
2025-02-01 19:52:55 +0100

    stable version

--- sketch.js
+++ sketch.js
@@ -1,44 +1,68 @@
-let angle = 0;
-let angle_2 = 0;
+// Variables para las rotaciones de cada planeta
+let angle_sun = 0;
+let angle_mercury = 0;
+let angle_venus = 0;
+let angle_earth = 0;
+let angle_moon = 0;
+let angle_mars = 0;
+let angle_jupiter = 0;
+let angle_saturn = 0;
+let angle_uranus = 0;
+let angle_neptune = 0;
+
+const earthSize = 100;  // Tamaño de la Tierra
+const moonSize = earthSize * 0.27; // Tamaño de la Luna (27% del tamaño de la Tierra)
+const moonDistance = earthSize * 40; // Distancia entre Tierra y Luna (40 radios terrestres)
 
 // Variables camara
 let cam;
-let camX = 0, camY = 0, camZ = 1000; // Posición inicial de la cámara
-let camRotX = 0, camRotY = 0; // Rotación acumulada de la cámara
+let camX = 0,
+  camY = 0,
+  camZ = 1000; // Posición inicial de la cámara
+let camRotX = 0,
+  camRotY = 0; // Rotación acumulada de la cámara
 let dragging = false; // Estado: si se está arrastrando el ratón
 
+function preload() {
+  sun_img = loadImage("./img/solete2k.jpg");
+  mercury = loadImage("./img/Mercurio2K.jpg");
+  venus = loadImage("./img/Venus.jpg");
+  venus_atm = loadImage("./img/VenusAtm.jpg");
+  earth = loadImage("./img/8k_earth_daymap.jpg");
+  earth_clouds = loadImage("./img/earth_clouds_2048.png");
+  moon = loadImage("./img/moon-texture.jpg");
+  mars = loadImage("./img/marte.jpg");
+  jupiter = loadImage("./img/jupiter.jpg");
+  saturn = loadImage("./img/saturno.jpg");
+  saturn_ring = loadImage("./img/saturn_ring.png");
+  uranus = loadImage("./img/uranus.jpg");
+  neptune = loadImage("./img/neptuno.jpg");
 
-function preload(){
-    img = loadImage('./img/8k_earth_daymap.jpg');
-    img_2 = loadImage('./img/earth_clouds_2048.png');
-    img_3 = loadImage('./img/moon-texture.jpg');
-    img_4 = loadImage('./img/background-space.jpg');
-    makemake_map = loadImage('./img/makemake.jpg'); 
-    death_star = loadImage('./img/death_star_1.png');
-    mistery =createVideo('./img/points.mp4');
-  }
+  space = loadImage("./img/background-space.jpg");
+  makemake_map = loadImage("./img/makemake.jpg");
+  death_star = loadImage("./img/death_star_1.png");
+  mistery = createVideo("./img/points.mp4");
+}
 
-console.log('esto rula!');
+console.log("esto rula!");
 
 function setup() {
-    createCanvas(windowWidth, windowHeight, WEBGL);
-    cam = createCamera();
-    mistery.hide();
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  cam = createCamera();
+  mistery.hide();
+ 
 }
-  
-  
-function draw() {
 
+function draw() {
   const speed = 15;
 
   if (keyIsDown(87)) cam.move(0, 0, -speed); // W
-  if (keyIsDown(83)) cam.move(0, 0, speed);  // S
+  if (keyIsDown(83)) cam.move(0, 0, speed); // S
   if (keyIsDown(65)) cam.move(-speed, 0, 0); // A
-  if (keyIsDown(68)) cam.move(speed, 0, 0);  // D
+  if (keyIsDown(68)) cam.move(speed, 0, 0); // D
 
   // Limitar el ángulo vertical de la cámara
   camRotX = constrain(camRotX, -PI / 2, PI / 2);
-
 
   // Actualizar rotación de la cámara
   if (dragging) {
@@ -46,53 +70,69 @@ function draw() {
     cam.tilt(camRotX); // Rotación vertical
   }
 
-  angle += 0.05; // Velocidad de rotación de la Tierra
-  angle_2 -= 0.3; // Velocidad de rotación de la Luna
+  //* Crear como constantes o ir declarandolas a medida que se vayan necesitando
+  // Actualizar ángulos de rotación de los planetas
+  angle_sun += 0.01; // Rotación del Sol
+  angle_mercury += 0.1; // Rotación de Mercurio
+  angle_venus += 0.08; // Rotación de Venus
+  angle_earth += 0.05; // Rotación de la Tierra
+  angle_moon += 0.09; // Rotación de la Tierra
+  angle_mars += 0.03; // Rotación de Marte
+  angle_jupiter += 0.02; // Rotación de Júpiter
+  angle_saturn += 0.01; // Rotación de Saturno
+  angle_uranus += 0.007; // Rotación de Urano
+  angle_neptune += 0.005; // Rotación de Neptuno
 
-  // Tierra
-  earth = () => sphere(300,200);
-  clouds = () => sphere(305,200);
-  earth_texture = () => texture(img_2);
-
-  //Luna
-  moon = () => sphere(40,100);
-  moon_texture = () => texture(img_3);
   
-  // Luces y fondo
-  lights();
-  background(0,0,0,0);
+  //Luces
+  background(0, 0, 0, 0);
+  lights()
+  pointLight(255, 255, 255, 0, 0, 0);
+  pointLight(160, 160, 160, -1000, -1000, 500);
 
-  // Luces direccionales
-  let c = color (255,255,255);
-  directionalLight(c, createVector(0,1,0));
-  directionalLight(c, createVector(1,0,0));
-  pointLight(160,160,160,-1000,-1000,500);
+  noStroke();  
 
-  ambientMaterial(5,5,5);
-  noStroke();
 
-  rotateY(radians(angle));
-  texture(img);
-  earth();
-
-  rotateY(radians(angle)/3);
-  earth_texture();
-  clouds();
-  
-  rotateY(radians(angle_2)/2);
-  moon_texture();
-  translate(-600, 0);
-  moon();
+  // Creación planetas
+  createPlanet(150, 150, sun_img, angle_sun, 0, 0, true);
+  createPlanet(15, 15, mercury, angle_mercury, 300, 0, true);
+  createPlanet(25, 25, venus, angle_venus, 450, 0, true);
+  createPlanet(27, 25, venus_atm, angle_venus, 450, 0, true);
+  createPlanet(30, 30, earth, angle_earth, 600, 0, true);
+  createPlanet(31, 30, earth_clouds, angle_earth, 600, 0, false);
+  createPlanet(17, 17, moon, angle_moon, 600, 0, true);
+  createPlanet(20, 20, mars, angle_mars, 750, 0, true);
+  createPlanet(100, 100, jupiter, angle_jupiter, 1100, 0, true);
+  createPlanet(85, 85, saturn, angle_saturn, 1400, 0, true);
+  createPlanet(89, 89, saturn_ring, angle_saturn, 1400, 0, true);
+  createPlanet(70, 70, uranus, angle_uranus, 1700, 0, true);
+  createPlanet(65, 65, neptune, angle_neptune, 2000, 0, true);
 
   smooth();
 }
+
+
+// Funcion para generar los planetas
+const createPlanet = (sizeX, sizeY, img, angle, translateX, translateY, followOrbit) => {
+  if (followOrbit){
+    push();    
+  } 
+  rotateY(radians(angle));
+  translate(translateX, translateY);
+  texture(img);
+  sphere(sizeX, sizeY);
+  if (followOrbit){
+    pop();
+  }
+  console.log("yessss");
+};
+ 
 
 // Cambia la rotación de la cámara al arrastrar el ratón
 function mouseDragged() {
   const sensitivity = 0.001; // Sensibilidad del arrastre
   camRotY = (mouseX - pmouseX) * sensitivity;
-  camRotX = (mouseY - pmouseY) * sensitivity;
-
+  //camRotX = (mouseY - pmouseY) * sensitivity;
   dragging = true;
 }
 
@@ -100,44 +140,6 @@ function mouseDragged() {
 function mouseReleased() {
   dragging = false;
   // Resetear rotaciones para detener el movimiento
-  camRotX = 0; 
+  camRotX = 0;
   camRotY = 0;
 }
-
-//! Mierda, No borrar
-
-
-
-    // earth_sphere_check = select('#earth_sphere_check').elt;
-    // earth_trump_style_check = select('#earth_trump_style_check').elt;
-    // earth_makemake_check = select('#earth_makemake_check').elt;
-    // moon_sphere_check = select('#moon_sphere_check').elt;
-    // moon_death_star_check = select('#moon_death_star_check').elt;
-    // moon_mistery_check = select('#moon_mistery_check').elt;
-
-
-
-
-
-  // if (earth_trump_style_check.checked) {
-  //   earth = () => ellipsoid(300,50,300,100);
-  //   clouds = () => ellipsoid(305,50,300,300);
-  //   earth_texture = () => texture(img_2);
-  // }
-
-  // if (earth_makemake_check.checked) {
-  //   earth = () => sphere(100,200);
-  //   clouds = () => sphere(105,200);
-  //   earth_texture = () => texture(makemake_map);
-  // }
-
-  // if(moon_death_star_check.checked) {
-  //   moon = () => sphere(40,100);
-  //   moon_texture = () => texture(death_star);
-  // }
-
-  // if(moon_mistery_check.checked) {
-  //   mistery.loop();
-  //   moon = () => sphere(40,100);
-  //   moon_texture = () => texture(mistery);
-  // }
